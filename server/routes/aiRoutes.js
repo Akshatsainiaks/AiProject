5c06919a443a3785cb8f1d5bcfd6bb27f7266c11
Akshatsainiaks <akshatsaini336@gmail>
2025-07-12 13:56:23 +0530

    project completed

---
+++ server/routes/aiRoutes.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import { auth } from '../middlewares/auth.js';
+import { genrateArticle, genrateBlogTitle, genrateImage, removeImageBackground, removeImageObject, resumeReview } from '../controllers/aiController.js';
+import { upload } from '../configs/multer.js';
+
+
+const aiRouter = express.Router();
+
+aiRouter.post('/generate-article',auth,genrateArticle)
+aiRouter.post('/generate-blog-title',auth,genrateBlogTitle)
+aiRouter.post('/generate-image',auth,genrateImage)
+aiRouter.post('/remove-image-background', upload.single('image'), auth,removeImageBackground)
+aiRouter.post('/remove-image-object', upload.single('image'), auth,removeImageObject)
+aiRouter.post('/resume-review', upload.single('resume'), auth,resumeReview)
+
+
+
+
+
+
+
+
+export default aiRouter;
