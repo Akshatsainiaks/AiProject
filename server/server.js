6472e5ce6913e3c097aa4cb1fd819b8bda5d3e1f
Akshatsainiaks <akshatsaini336@gmail>
2025-07-12 14:22:52 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -1,27 +1,28 @@
-// 
-
-// server.js
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { clerkMiddleware, requireAuth } from '@clerk/express';
+import { clerkMiddleware, requireAuth } from '@clerk/express'
 import aiRouter from './routes/aiRoutes.js';
 import connectCloudinary from './configs/cloudinary.js';
 import userRouter from './routes/userRoutes.js';
-import { createServerlessExpressHandler } from '@vendia/serverless-express';
 
-// Init app
-const app = express();
+
+
+const app=express();
 await connectCloudinary();
 
-app.use(cors());
-app.use(express.json());
-app.use(clerkMiddleware());
-app.use(requireAuth());
+app.use(cors())
 
-app.use('/api/ai', aiRouter);
-app.use('/api/user', userRouter);
-app.get('/', (req, res) => res.send('Server is Live!'));
+app.use(express.json())
+app.use(clerkMiddleware())
 
-// Export for Vercel
-export const handler = createServerlessExpressHandler({ app });
+app.use(requireAuth())
+app.use('/api/ai',aiRouter) 
+app.use('/api/user',userRouter)
+app.get('/',(req,res)=>res.send('Server is Live!'))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log("Server is running on PORT:",+PORT)
+})
