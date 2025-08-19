389b52d9a74b65cd1b684941f0e49955eb14e700
Akshatsainiaks <akshatsaini336@gmail.com>
2025-08-19 11:09:46 +0530

    new

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,6 +28,7 @@ const App = () => {
           <Route path='generate-images' element={<GenerateImages/>}/>
           <Route path='remove-background' element={<RemoveBackground/>}/>
           <Route path='remove-objects' element={<RemoveObject/>}/>
+          <Route path='remove-object' element={<RemoveObject/>}/>
           <Route path='review-resume' element={<ReviewResume/>}/>
           <Route path='community' element={<Community/>}/>
 
